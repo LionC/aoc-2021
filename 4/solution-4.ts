3511547966b1607bf7659b850e1ff9bf1cd6bdf0
Leon Strauss <me@lionc.de>
2021-12-04 21:44:57 +0100

    :sparkles: Day 4

---
+++ 4/solution-4.ts
@@ -0,0 +1,84 @@
+import { input } from "../utils.ts";
+import { chunk, findSingle, sumOf } from "../deps.ts";
+import "https://cdn.skypack.dev/@tensorflow/tfjs-backend-cpu";
+import * as tf from "https://cdn.skypack.dev/@tensorflow/tfjs-core";
+
+await tf.ready();
+
+// Parse input
+
+const BOARD_SIZE = 5;
+const parseNumber = (raw: string) => parseInt(raw);
+const [drawLine, _, ...boardLines] = input;
+
+const draws = drawLine
+  .split(",")
+  .map(parseNumber);
+
+const parsedBoardLines = boardLines
+  .filter((it) => it.length > 0)
+  .map((it) =>
+    it
+      .trim()
+      .split(/\s+/)
+      .map(parseNumber)
+  );
+
+const chunked = chunk(parsedBoardLines, BOARD_SIZE);
+const boardCount = chunked.length;
+
+// Solve
+
+const shape = [boardCount, BOARD_SIZE, BOARD_SIZE];
+const boards = tf.tensor3d(chunked, shape);
+let boardChecks = tf.fill(shape, false);
+
+function hasBoardWon(board: tf.Tensor): boolean {
+  const rows = tf.split(board, BOARD_SIZE, 0);
+  const columns = tf.split(board, BOARD_SIZE, 1);
+
+  return [...rows, ...columns]
+    .map((it: tf.Tensor) => tf.squeeze(it))
+    .map((it: tf.Tensor) => tf.all(it).arraySync())
+    .some((it: number) => it === 1);
+}
+
+const boardsDone = new Array<boolean>(boardCount).fill(false);
+let winnerCount = 0;
+let firstBoardWon = false;
+
+for (const draw of draws) {
+  const drawTensor = tf.fill(shape, draw);
+  const hits = tf.equal(drawTensor, boards);
+  boardChecks = tf.logicalOr(boardChecks, hits);
+
+  const unstacked = tf.unstack(boardChecks) as tf.Tensor[];
+  const wonIndexes = unstacked
+    .map((it, index) => [index, it] as const)
+    .filter(([index]) => !boardsDone[index])
+    .filter(([_, it]) => hasBoardWon(it))
+    .map(([index]) => index);
+
+  for (const wonIndex of wonIndexes) {
+    boardsDone[wonIndex] = true;
+    winnerCount += 1;
+
+    if (firstBoardWon && winnerCount !== boardCount) {
+      continue;
+    }
+
+    firstBoardWon = true;
+
+    const [winnerBoard, winnerChecks] = [boards, boardChecks]
+      .map((it) => tf.slice3d(it, wonIndex, [1]))
+      .map((it) => tf.squeeze(it));
+
+    const masked = await tf.booleanMaskAsync(
+      winnerBoard,
+      tf.logicalNot(winnerChecks),
+    );
+    const sum = tf.sum(masked).arraySync();
+
+    console.log(sum * draw);
+  }
+}
