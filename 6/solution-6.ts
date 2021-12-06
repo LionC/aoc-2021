f09b89a103fb0b680d8c99633a0a5d8c7fb43432
Leon Strauss <me@lionc.de>
2021-12-06 20:05:12 +0100

    :sparkles: Day 6

---
+++ 6/solution-6.ts
@@ -0,0 +1,31 @@
+import { input } from "../utils.ts"
+import { mapKeys, sumOf } from "../deps.ts"
+
+const DAYS_TO_SIMULATE = 256
+
+let generations = input[0]
+    .split(",")
+    .reduce<{ [age: string]: number }>(
+        (acc, cur) => ({
+            ...acc,
+            [cur]: (acc[cur] ?? 0) + 1,
+        }),
+        {},
+    )
+
+console.dir({ generations })
+
+for (let i = 0; i < DAYS_TO_SIMULATE; i++) {
+    const birthed = generations["0"] ?? 0
+    delete generations["0"]
+    generations["7"] = birthed + (generations["7"] ?? 0)
+    generations = mapKeys(generations, it => String(Number(it) - 1))
+    generations["8"] = birthed
+}
+
+const answer = sumOf(
+    Object.values(generations),
+    it => it,
+)
+
+console.log(answer)
--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.117.0/collections/mod.ts";
+export * from "https://deno.land/std/collections/mod.ts";
